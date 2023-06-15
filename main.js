4ac54a9ef07e8ea29e37b8aa952065fa31446829
Marcin Chołaściński <marcincholascinski@macbook-pro-marcin.home>
2023-06-15 23:57:57 +0200

    delete redundant files

--- main.js
+++ main.js
@@ -1,20 +1,80 @@
-let operation = "";
-const operators = ["+", "-", "/", "*"];
+const displayedNumber = document.querySelector(".displayed");
+const clearButton = document.querySelector(".clear");
+const operatorButton = document.querySelectorAll(".operator");
+const modButton = document.querySelectorAll(".mod");
+const numberButton = document.querySelectorAll(".number");
+const equalsButton = document.querySelector(".equals");
 
-const keys = document.querySelectorAll(".key");
-keys.forEach(key => {
-    key.addEventListener("click", () => {
-        const lastCharacter = operation.charAt(operation.length - 1);
-        if (operators.includes(key.innerHTML) && operators.includes(lastCharacter)) {
-            operation = operation.slice(0, -1) + key.innerHTML;
-        } else if (key.innerHTML === "=") {
-            document.querySelector(".result").textContent = String(eval(operation));
-            operation = ""
-        } else {
-            operation += key.innerHTML;
+displayedNumber.textContent = 0;
+let currentNumber = "";
+let result = "";
+let operator = "";
+
+function displayNumber() {
+    if (currentNumber === "0" && this.textContent === "0") {
+        return;
+    } else {
+        currentNumber += this.textContent;
+        displayedNumber.textContent = currentNumber;
+    }
+}
+
+function modifyNumber() {
+    if (currentNumber === "") {
+        return
+    } else {
+        switch (this.textContent) {
+            case "+ / -":
+                currentNumber = Number(currentNumber) * -1;
+                break;
+            case "%":
+                currentNumber = Number(currentNumber) / 100;
+                break;
         }
-        showOperation(operation);
-    })
-    })
+        displayedNumber.textContent = currentNumber;
+    }}
 
-const showOperation = operation => document.querySelector(".operation").textContent = operation;
+function operate() {
+    if (result != "") {
+        showResult();
+        operator = this.textContent;
+    } else if (currentNumber === "") {
+        return;
+    } else {
+        operator = this.textContent;
+        result = currentNumber;
+        currentNumber = "";
+        displayedNumber.textContent = result;
+    }
+}
+
+function showResult() {
+    switch (operator) {
+        case "+":
+            result = Number(result) + Number(currentNumber);
+            break;
+        case "-":
+            result = Number(result) - Number(currentNumber);
+            break;
+        case "*":
+            result = Number(result) * Number(currentNumber);
+            break;
+        case "/":
+            result = Number(result) / Number(currentNumber);
+            break;
+    }
+    displayedNumber.textContent = result;
+    currentNumber = "";
+}
+
+function clear() {
+    currentNumber = "";
+    result = "";
+    displayedNumber.textContent = 0;
+}
+
+operatorButton.forEach((button) => button.addEventListener("click", operate));
+numberButton.forEach((button) => button.addEventListener("click", displayNumber));
+modButton.forEach((button) => button.addEventListener("click", modifyNumber));
+equalsButton.addEventListener("click", showResult);
+clearButton.addEventListener("click", clear);
